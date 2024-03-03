14edceb99bfac31d9a10ad0ab8afd224ecbd68a0
Nirav Shah <niravshah30@outlook.com>
2024-03-03 21:46:57 +0530

    cart page changes

--- src/components/cart/CartPage.jsx
+++ src/components/cart/CartPage.jsx
@@ -1,10 +1,22 @@
 import { useSelector } from "react-redux";
 import { cartProducts } from "../../slice/cartSlice";
+import CartTile from "./CartTile";
 
 const CartPage = () => {
   const cart = useSelector(cartProducts);
   console.log(cart);
-  return <div>CartPage</div>;
+  return (
+    <>
+      <div className="container min-h-[80vh]">
+        <div className="grid md:grid-cols-2 gap-4">
+          <div className="p-6">
+            <CartTile />
+          </div>
+          <div>bill</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CartPage;
--- src/components/cart/CartTile.jsx
+++ src/components/cart/CartTile.jsx
@@ -1,5 +1,26 @@
+import { Avatar, Card, CardContent } from "@mui/material";
+
 const CartTile = () => {
-  return <div>CartTile</div>;
+  return (
+    <>
+      <div>
+        <Card className="md:w-[90%] w-full" elevation={8}>
+          <CardContent className="flex md:flex-row flex-col items-center gap-x-4">
+            <Avatar sx={{ width: 200, height: 100 }} variant="square">
+              <img
+                src="https://cdn.dummyjson.com/product-images/2/thumbnail.jpg"
+                alt="name"
+              />
+            </Avatar>
+            <p>Title</p>
+            <p>price</p>
+            <p>quantity</p>
+            <p>dlete</p>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default CartTile;
